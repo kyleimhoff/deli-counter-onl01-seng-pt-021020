636d309ca73e37bb4fa1ca5a4d79a1c21ca34de7
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-18 22:29:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-katz_deli = []
+katz_deli = ["Sarah", "Steve", ""]
 def line(array) 
   if array.length >= 1 
     new_array = []
@@ -12,7 +12,9 @@ def line(array)
   end
 end
 
-def take_a_number(katz_deli, new_customer)
+def take_a_number(line, new_customer)
+  line << new_person
+  puts "Welcome, #{new_person} you are number #{line.lenght"
 end
  
   
