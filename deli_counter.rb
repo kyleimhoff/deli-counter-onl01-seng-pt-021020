005f101fec0bd3196cbe08a9e124d54e460d8dba
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-18 22:24:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = []
-def line(array) 
-  if array.length >= 1 
+def line(katz_deli) 
+  if katz_deli.length >= 1 
     new_array = []
     counter = 1 
-    array.each do |name|
+    katz_deli.each do |name|
       new_array << "#{counter}. #{name}"
       counter += 1 
     end
@@ -11,5 +11,8 @@ def line(array)
    puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, new_customer)
+  
  
   
