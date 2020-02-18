b970e73d8ca4df2c8d1f8e3fc625b043c8911348
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-18 21:43:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 katz_deli = []
 def line(katz_deli) 
-  if katz_deli.length < 0 
-    puts "The line is currently: " +
+  if katz_deli.length > 0
+    katz_deli.each do |name, index|
+      puts
   
   
