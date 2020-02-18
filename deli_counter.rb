3025bf429b65738d34fd442ba8999e19beffd97e
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-18 22:57:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,14 +15,14 @@ end
 
 def take_a_number(line, new_customer)
   line << new_customer
-  puts "Welcome, #{new_customer} you are number #{line.length} in line."
+  puts "Welcome, #{new_customer}. You are number #{line.length} in line."
 end
  
 def now_serving(line)
   if line.length == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts "Cuttently serving : #{line[0]}."
+    puts "Cuttently serving #{line[0]}."
     line.shift
   end
 end
