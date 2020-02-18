41dc0029a51e4c31e9d5e7ed5dedc045d2d9ead2
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-18 22:33:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
       new_array << "#{counter}. #{name}"
       counter += 1 
     end
-    puts "The line is currntly: "
+    puts "The line is currntly: #{array.join(" ")}"
   else
    puts "The line is currently empty."
   end
