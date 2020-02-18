5f2913ba934fb1430cda19d1f4c1fe3dca05fcad
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-18 22:25:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = []
-def line(katz_deli) 
-  if katz_deli.length >= 1 
+def line(array) 
+  if array.length >= 1 
     new_array = []
     counter = 1 
-    katz_deli.each do |name|
+    array.each do |name|
       new_array << "#{counter}. #{name}"
       counter += 1 
     end
